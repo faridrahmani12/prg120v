d7e732fc415b66b2527984a258c64fa3dae4a533
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-21 19:29:20 +0000

    hei

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <body>
   <div class="page">
     <div class="card">
-      <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Universitetet_i_S%C3%B8r%C3%B8st-Norge_logo.svg/512px-Universitetet_i_S%C3%B8r%C3%B8st-Norge_logo.svg.png" alt="Universitetet i Sørøst-Noreg">
       <h1>PRG120V – Administrasjon</h1>
       <p>Velkommen! Velg hva du vil vedlikeholde.</p>
       <ul>
